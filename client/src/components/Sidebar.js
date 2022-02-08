b4fb6ced39211bba969b8fa600d7fd33aa803236
Dmitry <purpl3mint@yandex.ru>
2022-02-08 22:09:15 +0700

    Changed table of content and fixed keys for rows in table

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -16,31 +16,28 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import TablePagination from '@mui/material/TablePagination';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, type) {
+  return { name, type};
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Frozen yoghurt', 159, 6.0, 24, 14.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Frozen yoghurt', 159, 6.0, 24, 24.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Frozen yoghurt', 159, 6.0, 24, 34.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('La-La-La', 'Video'),
+  createData('Somebodys watching me', 'URL'),
+  createData('Dog', 'Image'),
+  createData('Schedule', 'Text'),
+  createData('La-La-La', 'Video'),
+  createData('Somebodys watching me', 'URL'),
+  createData('Dog', 'Image'),
+  createData('Schedule', 'Text'),
+  createData('La-La-La', 'Video'),
+  createData('Somebodys watching me', 'URL'),
+  createData('Dog', 'Image'),
+  createData('Schedule', 'Text'),
+  createData('La-La-La', 'Video'),
+  createData('Somebodys watching me', 'URL'),
+  createData('Dog', 'Image'),
+  createData('Somebodys watching me', 'URL'),
+  createData('Dog', 'Image'),
 ];
 
 function ContentTable() {
@@ -63,27 +60,21 @@ function ContentTable() {
         <Table sx={3} size="small" aria-label="a dense table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Type</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            .map((row) => (
+            .map((row, index) => (
               <TableRow
-                key={row.name}
+                key={index}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
                   {row.name}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.type}</TableCell>
               </TableRow>
             ))}
           </TableBody>
