625836754a0aa432608eeec3d900428d922e5514
Dmitry <purpl3mint@yandex.ru>
2022-02-28 23:56:35 +0700

    Added materailize css and react-helmet for client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 //import logo from './logo.svg';
 import './App.css';
+import 'materialize-css'
+import { HeadTag } from './components/HeadTag';
 import { BrowserRouter as Router} from 'react-router-dom';
 import { useAuth } from './hooks/auth.hook';
 import { useRoutes } from './routes';
@@ -15,6 +17,7 @@ function App() {
   return (
     <AuthContext.Provider value={{token, login, logout, isAuthenticated}}>
       <Router>
+        <HeadTag />
         { isAuthenticated && <Navbar /> }
         <div className='wrapper'>
           {routes}
@@ -22,26 +25,6 @@ function App() {
       </Router>
     </AuthContext.Provider>
   )
-  /*
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-  */
 }
 
 export default App;
---
+++ client/src/components/HeadTag.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+export const HeadTag = () => {
+    return (
+        <Helmet>
+            <title>NSTU BS</title>
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+        </Helmet>
+    )
+}
