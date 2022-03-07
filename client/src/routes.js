def3aaed01eaf2be5a658141728377d09ba2396b
Dmitry <purpl3mint@yandex.ru>
2022-03-07 17:51:37 +0700

    Refactored client routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -10,27 +10,7 @@ import { UsersPage } from "./pages/Users/UsersPage"
 import { Sidebar } from "./components/Sidebar"
 
 export const useRoutes = (isAuthenticated) => {
-  
-  let routesList = [
-    {comp:<AuthPage />,                  path:"/",                           exact:true,  auth:false},
-    {comp:<StartPage />,                 path:"/",                           exact:false, auth:true},
-    {comp:<BannersPage />,               path:"/banners",                    exact:true,  auth:true},
-    {comp:<ContentsPage />,              path:"/contents",                   exact:true,  auth:true},
-    {comp:<MediaplansPage />,            path:"/mediaplans",                 exact:true,  auth:true},
-    {comp:<TickersPage />,               path:"/tickers",                    exact:true,  auth:true},
-    {comp:<UsersPage />,                 path:"/users",                      exact:true,  auth:true},
-  ]
 
-  let routesDisplay = routesList.map(r => {
-      if (isAuthenticated === r.auth)
-          if (r.exact === true)
-              return <Route path={r.path} element={r.comp} key={r.comp + r.path} exact/>
-          else
-              return <Route path={r.path} element={r.comp} key={r.comp + r.path} />
-      return <Route key={r.comp + r.path}/>
-  })
-  
-  //{routesDisplay}
   return (
     <div className="row">
         
@@ -59,12 +39,3 @@ export const useRoutes = (isAuthenticated) => {
     </div>
   )
 }
-
-/*
-              {
-                (isAuthenticated) && <Route path="/" element={<StartPage/>} />
-              }
-              {
-                (!isAuthenticated) && <Route path="/" element={<AuthPage/>} />
-              }
-              */
